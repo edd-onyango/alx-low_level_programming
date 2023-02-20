df8da3cb35c37fb551a8a5eccd5c5bb97f8beba7
edd-onyango <eddonyango.dev@gmail.com>
2023-02-20 14:00:26 -0800

    Print alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	int i;
+
+	for (i = 0; i < 26; i++)
+
+	{
+
+		putchar(alpha[i]);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
+
+
