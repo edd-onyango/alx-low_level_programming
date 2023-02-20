c367aeb68c5f47a66ed57c83eea946ad936abd70
edd-onyango <eddonyango.dev@gmail.com>
2023-02-20 14:24:36 -0800

    print number 1 through 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers 1-10
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	int i;
+
+	for (i = 0; i <= 10; i++)
+
+	{
+
+		printf("%d", i);
+
+	}
+
+	printf("\n");
+
+	return (0);
+
+}
+
+
