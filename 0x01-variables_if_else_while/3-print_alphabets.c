acbf0023cad70687eb0d6a477c4443f57449f2d4
edd-onyango <eddonyango.dev@gmail.com>
2023-02-20 14:10:34 -0800

    print alphabets upper and lower.

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet upper/lower.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	char alpha[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	int i;
+
+	for (i = 0; i < 52; i++)
+
+	{
+
+		putchar(alpha[i]);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
+
+
