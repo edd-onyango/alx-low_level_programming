f4622fafa867f7fb1a756deed09eda53f1d0d4e0
edd-onyango <eddonyango.dev@gmail.com>
2023-02-20 14:52:38 -0800

    print combination of numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - combinationa of numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	int i;
+
+	for (i = 48; i < 58; i++)
+
+	{
+
+		putchar(i);
+
+		if (i != 57)
+
+		{
+
+			putchar(',');
+
+			putchar(' ');
+
+		}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
+
