887c0b63c51240518609b62d63152d53bef646a8
edd-onyango <eddonyango.dev@gmail.com>
2023-02-21 14:04:58 -0800

    print the alphabet in lowercase 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
